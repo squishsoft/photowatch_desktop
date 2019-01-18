038dfbe314acf1837aae911c7a6dd6df96b6e001
Sam Gerrard <fireproofpenguin@gmail.com>
2019-01-18 09:42:16 +0000

    Added add customer, and empty calendar view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
+import { CustomerNewComponent } from './components/customer-new/customer-new.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'customers',
+        component: HomeComponent
+    },
+    {
+        path: 'customers/new',
+        component: CustomerNewComponent
+    },
+    {
+        path: 'customers/:id',
+        component: CustomerDetailsComponent
+    },
+    {
+        path: "calendar",
+        component: CalendarComponent
     }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,9 @@ import { WebviewDirective } from './directives/webview.directive';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
+import { CustomerNewComponent } from './components/customer-new/customer-new.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -28,7 +31,10 @@ export function HttpLoaderFactory(http: HttpClient) {
   declarations: [
     AppComponent,
     HomeComponent,
-    WebviewDirective
+    WebviewDirective,
+    CustomerDetailsComponent,
+    CustomerNewComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
